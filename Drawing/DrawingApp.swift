0e3c7dda10c5a278807911de9211cf60fd3c5eb7
Patricia <patysiq@yahoo.com.br>
2021-01-29 22:30:06 -0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by PATRICIA S SIQUEIRA on 29/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
